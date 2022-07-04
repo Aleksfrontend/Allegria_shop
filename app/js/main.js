873e3cf16670b7cfdc224b0ed7807e797a67ee3b
Doroshenko Aleksandr <doroshenko.bey@gmail.com>
2022-07-04 22:00:29 +0300

    Record

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.slider-offer').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
+	});
+
+	// $('.slider-reviews').slick({
+	// 	slidesToShow: 1,
+	// 	infinite: true,
+	// 	speed: 500,
+	// 	autoplay: false,
+	// 	responsive: [
+	// 		{
+	// 			breakpoint: 768,
+	// 			settings: {
+	// 				arrows: false,
+	// 				dots: true,
+	// 			}
+	// 		}
+	// 	]
+	// });
+})
